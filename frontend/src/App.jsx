bcf169379b840855105f950eb28c5419ffe9f96d
Jeremy Nyangi <justinjerome401@gmail.com>
2025-05-14 11:29:29 +0300

    fix(css): tailwind and index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-
-
-const MovieCard = ({title, rating, actors}) => {
-  return (
-    <div className="movie-card">
-      <p>
-        {title} - {rating} stars. The actor{(actors ?? []).length > 1? 's':''} of this movie {(actors ?? []).length === 1 ? 'is' : 'are'} {' '}
-        {(actors ?? []).map((actor) => actor.actor_name).join(', ') || 'N/A'} 
-        </p>
-    </div>
-  );
-}
+import React from 'react'
 
 const App = () => {
-  return(
-  
-  <>
-  <h2>Hello</h2>
-  <MovieCard title={'SpiderMan'} rating={3} actors={[{actor_name:'Tobey MaGuire'},{actor_name:'Natasha Romanoff'}]} />
-  <MovieCard title={'Avengers Endgame'} rating={4} actors={[{actor_name:'Robert Downey Junior'}]}/>
-  <MovieCard title={'Moon Knight'} rating={3} actors={[{actor_name:'Oscar Isaac'}]}/>
-  <MovieCard title={'Sentry'} rating={2} actors={[{actor_name:'Chris Evans'}]}/>
-  <MovieCard title={'Avengers Doomsday'} rating={4} actors={[{actor_name:'Letitia Wright'},{actor_name:'Benedict Cumberbatch'}]}/>
-
-  </>    
-  );
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <h1>Lights. Camera. <span className="text-gradient">Stream !</span> </h1>
+        </header>
+      </div>
+    </main>
+  )
 }
 
 export default App
