40b8ed39bddc4e62e5139e436d1cc3ea8a0d0056
Jeremy Nyangi <justinjerome401@gmail.com>
2025-05-14 12:42:42 +0300

    chore(frontend):Search Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/search'
 
 const App = () => {
+  const[searchTerm, setsearchTerm] = useState('');
   return (
     <main>
       <div className="pattern" />
       <div className="wrapper">
         <header>
+          <img src="./hero.png" alt="Hero Banner" />
           <h1>Lights. Camera. <span className="text-gradient">Stream !</span> </h1>
         </header>
+        <Search searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
+        <h1>{searchTerm}</h1>
       </div>
     </main>
   )
