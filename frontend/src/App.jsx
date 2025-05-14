70e1da005918a1fabc536cfd6c4c3a546af16ce5
Jeremy Nyangi <justinjerome401@gmail.com>
2025-05-15 01:13:17 +0300

    chore(frontend): Trending Movies List

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -82,6 +82,15 @@ const App = () => {
         {trendingMovies.length>0 && (
           <section className="trending">
             <h2>Trending Movies</h2>
+
+            <ul>
+            {trendingMovies.map((movie,index)=>(
+              <li key={movie.$id}>
+                <p>{index+1}</p>
+                <img src={movie.poster_url} alt={movie.title} />
+                </li>
+            ))}
+            </ul>
           </section>
         )}
 
